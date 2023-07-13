9c2b55776158630b4939c105646bbbde610c1620
Bhavya Abhaykumar Jain <138629017+BhavyaJain010@users.noreply.github.com>
2023-07-13 18:57:11 -0400

    Update Header.js

--- Src/Components/Header/Header.js
+++ Src/Components/Header/Header.js
@@ -1,1 +1,25 @@
+import logo from './logo.svg';
+import rocket from './rocket.png';
+import schedules from './Schedules.png';
+import members from './Members.png';
+import frame from './Frame.png';
+import './App.css';
+
+function App() {
+  return (
+    <header className='App-header'>
+  <div class="button-container">
+    <img src={rocket} class='rocket_img'/>
+    <button class="my-button">Missions</button>
+    <img src={schedules} class='schedules_img'/>
+    <button class="my-button">Leaderbord</button>
+    <img src={members} class='members_img'/>
+    <button class="my-button">Rewards</button>
+    <img src={frame} class='frame_img'></img>
+  </div>
+</header>
+  );
+}
+
+export default App;
 
